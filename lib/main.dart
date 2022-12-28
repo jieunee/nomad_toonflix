1adb6a76e98356b8df9d533bdcb6196d0b0fa77e
Green LEE <green@with.green>
2022-12-28 17:10:19 +0900

    add CustomScroolBehavior

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
 }
 
 class App extends StatelessWidget {
@@ -11,6 +21,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scrollBehavior: MyCustomScrollBehavior(),
       home: HomeScreen(),
     );
   }
